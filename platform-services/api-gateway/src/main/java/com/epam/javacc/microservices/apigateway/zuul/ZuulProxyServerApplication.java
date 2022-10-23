841eae0b61fcce7a3f06c4306f033b9e06ed7612
Roman <kovalchuk.roman03@gmail.com>
2022-10-24 00:18:33 +0300

    Added eureka-client.

--- platform-services/api-gateway/src/main/java/com/epam/javacc/microservices/apigateway/zuul/ZuulProxyServerApplication.java
+++ platform-services/api-gateway/src/main/java/com/epam/javacc/microservices/apigateway/zuul/ZuulProxyServerApplication.java
@@ -3,11 +3,12 @@ package com.epam.javacc.microservices.apigateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ZuulProxyServerApplication {
 
     public static void main(String[] args) {
