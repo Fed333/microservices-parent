d0076bbbf729d9b7c8a9f023784b6be43f927e3c
Roman <kovalchuk.roman03@gmail.com>
2022-10-24 00:15:12 +0300

    Configured zuul proxy.

---
+++ platform-services/api-gateway/src/main/java/com/epam/javacc/microservices/apigateway/zuul/ZuulProxyServerApplication.java
@@ -0,0 +1,17 @@
+package com.epam.javacc.microservices.apigateway.zuul;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+//@EnableEurekaClient
+public class ZuulProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyServerApplication.class, args);
+    }
+
+}
