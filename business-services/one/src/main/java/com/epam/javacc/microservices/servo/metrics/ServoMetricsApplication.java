7c64e10ae93f59aafff8f5ca955b314bd91e9ef7
Roman <kovalchuk.roman03@gmail.com>
2022-12-04 12:59:34 +0200

    Registered service one as eureka client.

--- business-services/one/src/main/java/com/epam/javacc/microservices/servo/metrics/ServoMetricsApplication.java
+++ business-services/one/src/main/java/com/epam/javacc/microservices/servo/metrics/ServoMetricsApplication.java
@@ -3,7 +3,9 @@ package com.epam.javacc.microservices.servo.metrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServoMetricsApplication {
 
